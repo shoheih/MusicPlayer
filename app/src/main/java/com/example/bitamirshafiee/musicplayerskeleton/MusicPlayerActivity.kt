fae9dd403556483aff128213692e0286b7817e31
shoheih <shoheigithub@gmail.com>
2019-05-06 11:22:55 +0900

    Read Musics from Device Storage

--- app/src/main/java/com/example/bitamirshafiee/musicplayerskeleton/MusicPlayerActivity.kt
+++ app/src/main/java/com/example/bitamirshafiee/musicplayerskeleton/MusicPlayerActivity.kt
@@ -1,13 +1,22 @@
 package com.example.bitamirshafiee.musicplayerskeleton
 
 import android.content.pm.PackageManager
+import android.database.Cursor
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.support.v4.app.ActivityCompat
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.LinearLayoutManager
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_music_player.*
 
 class MusicPlayerActivity : AppCompatActivity() {
+
+    private lateinit var musicList: MutableList<Music>
+    private lateinit var linearLayoutManager: LinearLayoutManager
+    private lateinit var adapter: MusicAdapter
 
     companion object {
         private const val REQUEST_CODE_READ_EXTERNAL_STORAGE = 1
@@ -17,8 +26,40 @@ class MusicPlayerActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_music_player)
 
+        musicList = mutableListOf()
+
         if (Build.VERSION.SDK_INT >= 23)
             checkPermissions()
+    }
+
+    private fun getSongs() {
+
+        val selection = MediaStore.Audio.Media.IS_MUSIC
+        val projection = arrayOf(
+            MediaStore.Audio.Media.ARTIST,
+            MediaStore.Audio.Media.TITLE,
+            MediaStore.Audio.Media.DATA
+        )
+        val cursor : Cursor? = contentResolver.query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+            projection,
+            selection, null, null)
+
+        while (cursor!!.moveToNext()) {
+
+            musicList.add(Music(cursor.getString(0), cursor.getString(1), cursor.getString(2)))
+
+        }
+
+        cursor.close()
+
+        linearLayoutManager = LinearLayoutManager(this)
+
+        adapter = MusicAdapter(musicList)
+
+        recycler_view.layoutManager = linearLayoutManager
+
+        recycler_view.adapter = adapter
+
     }
 
     private fun checkPermissions() {
@@ -26,6 +67,7 @@ class MusicPlayerActivity : AppCompatActivity() {
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
 
             //read the songs
+            getSongs()
         } else {
             //false -> user asked not to ask me any more/permission disabled
             //true -> rejected before want to use the feature again
@@ -42,6 +84,7 @@ class MusicPlayerActivity : AppCompatActivity() {
             REQUEST_CODE_READ_EXTERNAL_STORAGE -> if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
                 //read songs
+                getSongs()
             } else {
                 Toast.makeText(this, "Permission Is not Granted", Toast.LENGTH_SHORT).show()
             }
