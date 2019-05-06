40430d46e296d727e524fafad8529be095bb6e55
shoheih <shoheigithub@gmail.com>
2019-05-06 10:08:11 +0900

    Initial commit

---
+++ app/src/main/java/com/example/bitamirshafiee/musicplayerskeleton/MusicPlayerActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bitamirshafiee.musicplayerskeleton
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MusicPlayerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_music_player)
+    }
+}
